0f7d05904662de8cba2114ad2be8db407e62d20a
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-07-25 01:51:28 +0300

    Positioning in progress

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include<Windows.h>
 #include<iostream>
 
@@ -5,34 +6,78 @@ using namespace std;
 
 namespace Geometry
 {
-	enum Color
+
+	enum Color //enum (Enumeration - Перечисление) - это набор именованных констрант типа 'int'
 	{
-		CONSOLE_RED = 0xCC,
+		RED			= 0x000000FF,
+		GREEN		= 0x0000FF00,
+		BLUE		= 0x00FF0000,
+		YELLOW		= 0x0000FFFF,
+
+		CONSOLE_RED = 0xCC,//старшая 'C' - цвет фона, младшая 'C' - цвет текста.
 		CONSOLE_GREEN = 0xAA,
 		CONSOLE_BLUE = 0x99,
 		CONSOLE_DEFAULT = 0x07
 	};
 
+#define SHAPE_TAKE_PARAMETERS unsigned int start_x, unsigned int start_y, unsigned int line_width, Color color
+#define SHAPE_GIVE_PARAMETERS start_x, start_y, line_width, color
+
 	class Shape
 	{
 	protected:
-
+		unsigned int start_x;
+		unsigned int start_y;
+		unsigned int line_width;
 		Color color;
 	public:
+		//Чисто-виртуальные функции (Pure virtual function)
 		virtual double get_area()const = 0;
 		virtual double get_perimeter()const = 0;
 		virtual void draw()const = 0;
-		Shape(Color color) :color(color) {}
-		virtual~Shape() {}
+
+		Shape(SHAPE_TAKE_PARAMETERS) :color(color)
+		{
+			set_start_x(start_x);
+			set_start_y(start_y);
+			set_line_width(line_width);
+		}
+		virtual ~Shape() {}
+
+		//////		Encapsulation		///////
+		unsigned int get_start_x()const
+		{
+			return start_x;
+		}
+		unsigned int get_start_y()const
+		{
+			return start_y;
+		}
+		unsigned int get_line_width()const
+		{
+			return line_width;
+		}
+		void set_start_x(unsigned int start_x)
+		{
+			this->start_x = start_x;
+		}
+		void set_start_y(unsigned int start_y)
+		{
+			this->start_y = start_y;
+		}
+		void set_line_width(unsigned int line_width)
+		{
+			this->line_width = line_width;
+		}
+		
 		virtual void info()const
 		{
-			cout << "площадь фигуры: " << get_area() << endl;
-			cout << "gthtvtnh фигуры: " << get_perimeter() << endl;
+			cout << "Площадь  фигуры: " << get_area() << endl;
+			cout << "Периметр фигуры: " << get_perimeter() << endl;
 			draw();
 		}
-
 	};
-	class Square :public Shape
+	/*class Square :public Shape
 	{
 		double side;
 	public:
@@ -80,19 +125,19 @@ namespace Geometry
 			cout << "Длина стороны: " << get_side() << endl;
 			Shape::info();
 		}
-	};
+	};*/
 	class Rectangle :public Shape
 	{
-		double width;
-		double height;
+		double width;	//ширина прямоугольника
+		double height;	//высота прямоугольника
 	public:
-		Rectangle(double width, double height, Color color) :Shape(color)
+		Rectangle(double width, double height, SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS)
 		{
 			set_widht(width);
 			set_height(height);
 		}
 		~Rectangle() {}
-		void set_widht(double widht)
+		void set_widht(double width)
 		{
 			this->width = width;
 		}
@@ -118,20 +163,29 @@ namespace Geometry
 		}
 		void draw()const override
 		{
-			//1
-			//HWND hwnd = GetActiveWindow();
-			HWND hwnd = FindWindow(NULL,L"Inhereritance - Microsoft Visual Studio");
-			//2
-			HDC hdc = GetDC(hwnd);
-			//3
-			HPEN hPen = CreatePen(PS_SOLID, 5, color);
-			HBRUSH hBrush = CreateSolidBrush(color);
-			//4
+			//WinGDI - Windows Grphics Device Interface
+			//1) Получаем окно консоли:
+			//HWND hwnd = GetConsoleWindow();	//Функция GetConsoleWindow() получает окно консоли
+			HWND hwnd = FindWindow(NULL, L"Inhereritance - Microsoft Visual Studio");
+			//2) Для того чтобы рисовать, нужен контекст устройства (Device Context), 
+					//	 который есть у каждого окна. Констекст устройства можно получить при помощи функции GetDC();
+			HDC hdc = GetDC(hwnd);	//Получаем контекст окна консоли
+			//Контекст устройства - это то, на чем мы будем рисовать.
+
+			//3) Теперь нам нужно то, чем мы будем рисовать:
+			HPEN hPen = CreatePen(PS_SOLID, line_width, color);	//hPen		- рисует контур фигуры;
+			//SP_SOLID	- сплошная линия
+			//5			- толщина линии 5 пикселов
+			HBRUSH hBrush = CreateSolidBrush(color);	//hBrush	- рисует заливку фигуры (SolidBrush - сплощной цвет)
+
+			//4) Выбираем чем, и на чем мы будем рисовать:
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
-			//5
-			::Rectangle(hdc, 100, 100, 900, 300);
-			//6
+
+			//5) Рисуем фигуру:
+			::Rectangle(hdc, start_x, start_y, start_x + width, start_y + height);	//:: - Global Scope (Глобальное пространство имен)
+
+			//6) hdc, hPen и hBrush занимают ресурсы, и после того, как мы ими воспользовались, ресурсы нужно освободить:
 			DeleteObject(hBrush);
 			DeleteObject(hPen);
 
@@ -145,18 +199,90 @@ namespace Geometry
 			Shape::info();
 		}
 	};
+
+	class Square :public Rectangle
+	{
+	public:
+		Square(double side, SHAPE_TAKE_PARAMETERS) :Rectangle(side, side, SHAPE_GIVE_PARAMETERS) {}
+		~Square() {}
+	};
+	class Circle :public Shape
+	{
+		double radius;
+	public:
+		Circle(double radius, SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS)
+		{
+			set_radius(radius);
+		}
+		~Circle()
+		{
+
+		}
+		void set_radius(double radius)
+		{
+			this->radius = radius;
+		}
+		double get_radius() const
+		{
+			return radius;
+		}
+		double get_diameter() const
+		{
+			return 2*radius;
+		}
+
+		double get_area()const override
+		{
+			return M_PI * radius * radius;
+		}
+		double get_perimeter() const override
+		{
+			return M_PI * get_diameter();
+		}
+		void draw()const override
+		{
+			HWND hwnd = FindWindow(NULL, L"Inhereritance - Microsoft Visual Studio");
+			HDC hdc = GetDC(hwnd);
+
+			HPEN hPen = CreatePen(PS_SOLID, 5, color);
+			HBRUSH hBrush = CreateSolidBrush(color);
+
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+
+			::Ellipse(hdc, start_x, start_y, start_x + get_diameter(), start_y + get_diameter());
+
+			DeleteObject(hBrush);
+			DeleteObject(hPen);
+
+			ReleaseDC(hwnd, hdc);
+		}
+		void info()const override
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "disk radius" << get_diameter() << endl;
+			cout << "disk diameter" << get_diameter() << endl;
+			Shape::info();
+		}
+	};
 }
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	//Shape shape(Color::CONSOLE_RED);
-	Geometry::Square square(5, Geometry::Color::CONSOLE_RED);
+	Geometry::Square square(5, 100, 100, 5, Geometry::Color::RED);
 	//cout << "Длина стороны: " << square.get_side() << endl;
 	//cout << "Площадь квадрата: " << square.get_area() << endl;
 	//cout << "Периметр квадрата: " << square.get_perimeter() << endl;
 	//square.draw();
 	square.info();
-	Geometry::Rectangle rect(100, 50, Geometry::Color::CONSOLE_BLUE);
+	Geometry::Rectangle rect(100, 50, 2000, 100, 10, Geometry::Color::BLUE);
 	rect.info();
-}
+
+	Geometry::Circle disk(100, 500, 100, 5, Geometry::Color::YELLOW);
+	disk.info();
+
+
+}	
+
