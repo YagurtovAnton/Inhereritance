f52981f800406c32096ac3c91f2e63babfb186ef
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-07-27 18:45:05 +0300

    Triangles

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -30,6 +30,15 @@ namespace Geometry
 		unsigned int start_y;
 		unsigned int line_width;
 		Color color;
+		static const int MIN_START_X = 100;
+		static const int MIN_START_Y = 50;
+		static const int MAX_START_X = 1000;
+		static const int MAX_START_Y = 500;
+		static const int MIN_LINE_WIDTH = 1;
+		static const int MAX_LINE_WIDTH = 32;
+		static const int MIN_SIZE = 50;
+		static const int MAX_SIZE = 500;
+		static int count;
 	public:
 		//Чисто-виртуальные функции (Pure virtual function)
 		virtual double get_area()const = 0;
@@ -41,8 +50,16 @@ namespace Geometry
 			set_start_x(start_x);
 			set_start_y(start_y);
 			set_line_width(line_width);
+			count++;
 		}
-		virtual ~Shape() {}
+		virtual ~Shape() 
+		{
+			count--;
+		}
+		int get_count()const
+		{
+			return count;
+		}
 
 		//////		Encapsulation		///////
 		unsigned int get_start_x()const
@@ -59,16 +76,30 @@ namespace Geometry
 		}
 		void set_start_x(unsigned int start_x)
 		{
+			if (start_x < MIN_START_X)start_x = MIN_START_X;
+			if (start_x > MAX_START_X)start_x = MAX_START_X;
 			this->start_x = start_x;
 		}
 		void set_start_y(unsigned int start_y)
 		{
+			if (start_y < MIN_START_Y)start_y = MIN_START_Y;
+			if (start_y > MAX_START_Y)start_y = MAX_START_Y;
 			this->start_y = start_y;
 		}
 		void set_line_width(unsigned int line_width)
 		{
 			this->line_width = line_width;
+			line_width < MIN_LINE_WIDTH ? MIN_LINE_WIDTH :
+			line_width > MAX_LINE_WIDTH ? MAX_LINE_WIDTH :line_width;
 		}
+		int filter_size(int size)const
+		{
+			return
+				size < MIN_SIZE ? MIN_SIZE :
+				size > MAX_SIZE ? MAX_SIZE :
+				size;
+		}
+		//////  метод////		 
 		
 		virtual void info()const
 		{
@@ -77,6 +108,8 @@ namespace Geometry
 			draw();
 		}
 	};
+
+	int Shape::count = 0;
 	/*class Square :public Shape
 	{
 		double side;
@@ -265,6 +298,18 @@ namespace Geometry
 			Shape::info();
 		}
 	};
+	class Triangle :public Shape
+	{
+	public:
+		Triangle(SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS){}
+		~Triangle() {}
+		virtual double get_height()const = 0;
+		void info()const override
+		{
+			cout << "Высота треугольника: " << get_height() << endl;
+			Shape::info();
+		}
+	};
 }
 
 void main()
@@ -283,6 +328,6 @@ void main()
 	Geometry::Circle disk(100, 500, 100, 5, Geometry::Color::YELLOW);
 	disk.info();
 
-
+	cout << "Количество фигур: " << disk.get_count() << endl;
 }	
 
